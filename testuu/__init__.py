f7f10475b3a396a52fff47b7528d5cc3d788eda8
Vincysuper07 <52707876+Vincysuper07@users.noreply.github.com>
2020-10-29 10:23:22 +0100

    Update __init__.py

--- testuu/__init__.py
+++ testuu/__init__.py
@@ -5,4 +5,4 @@ async def load():
     async with aiohttp.ClientSession() as session:
         async with session.get("https://raw.githubusercontent.com/Vincysuper07/StrapBot-testuu/master/qpowieurtyturiewqop.json") as req:
             async with aiofiles.open("testù.json", mode="w") as file:
-                json.dump(awair req.content.read(), file, indent=4)
+                json.dump(await req.content.read(), file, indent=4)
