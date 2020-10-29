2dd25d7b2038fa04cba9af87e9e6dcb2b604f25d
Vincysuper07 <52707876+Vincysuper07@users.noreply.github.com>
2020-10-29 10:08:03 +0100

    testù

---
+++ testù/__init__.py
@@ -0,0 +1,8 @@
+import json
+import aiohttp, aiofiles
+
+async def load():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://raw.githubusercontent.com/Vincysuper07/StrapBot-testuu/master/qpowieurtyturiewqop.json") as req:
+            async with aiofiles.open("testù.json", mode="w") as file:
+                json.dump(req.content.read(), file, indent=4)
